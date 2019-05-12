f624da5cf675b891c1998357f841d84a31fbd01a
Zuzana <centrummandala@gmail.com>
2019-05-12 11:26:10 +0200

    předpověď na 4 dny

--- src/predpoved.js
+++ src/predpoved.js
@@ -14,50 +14,72 @@ export default class Predpoved {
         fetch(`${forecastUrl}?APPID=${apiKey}&q=${location}&units=metric&lang=cz`)
             .then(response => response.json())
             .then(data => {
-                this.getForecastDate(data);       
+                this.displayForecast(data);       
         })
     }
 
-    getForecastDate(data){
-        console.log(data.filter(filterDate));
-    }
-    
-    
-
     filterDate(data){
         let dnes = new Date().getDate();
-        let datum = new Date(data.dt * 1000).getDate();
         let novePole = [];
+        let novePoleTemp1 = [];
+        let novePoleTemp2 = [];
+        let novePoleTemp3 = [];
+        let novePoleTemp4 = [];
 
-        if(datum == dnes + 1){
-            novePole.push(data);
-        };
-        console.log(novePole);
+            data.list.forEach (den => {
+                let datum = new Date(den.dt * 1000).getDate();
+                    if(datum == dnes+1){
+                        novePoleTemp1.push(den.main.temp); 
+                    }
+
+                    else if(datum == dnes+2){
+                        //novePole.push(den.main.temp);
+                        novePoleTemp2.push(den.main.temp); 
+                    }
+
+                    else if(datum == dnes+3){
+                        //novePole.push(den.main.temp);
+                        novePoleTemp3.push(den.main.temp); 
+                    }
+
+                    else if(datum == dnes+4){
+                        //novePole.push(den.main.temp);
+                        novePoleTemp4.push(den.main.temp); 
+                    };
+            })
+    
+            let maxTemp1 = Math.max(...novePoleTemp1);
+            let maxTemp2 = Math.max(...novePoleTemp2);
+            let maxTemp3 = Math.max(...novePoleTemp3);
+            let maxTemp4 = Math.max(...novePoleTemp4);
+    
+            data.list.forEach (den => {
+                if(den.main.temp == maxTemp1){
+                    novePole.push(den);
+                } else if(den.main.temp == maxTemp2){
+                    novePole.push(den);
+                } else if(den.main.temp == maxTemp3){
+                    novePole.push(den);
+                } else if(den.main.temp == maxTemp4){
+                    novePole.push(den);
+                }
+            })    
+
         return novePole;
-    }
-
-    filterTemperature(data){
-        //console.log(Math.max(data.list[0].main.temp));
-        return Math.max(data.list[0].main.temp);
+                
     }
 
     
-
-
     displayForecast(data){
-        
+         
         let predpoved = document.querySelector("#predpoved");
         let html = "";
-
-        data.filter(filterDate);
         
-        data.list.forEach(den => {
+        this.filterDate(data).forEach(den => {
             
             let datum = new Date(den.dt * 1000).getDate();
             let jmenoDne = new Date(den.dt * 1000).getDay();
             let dnes = new Date().getDate();
-
-            console.log(datum == dnes + 1)
 
             if(jmenoDne == 0){
                 jmenoDne = "neděle";
